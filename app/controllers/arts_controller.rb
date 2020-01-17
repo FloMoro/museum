6658a86f84bbadcde9de1d01af7c82099f4cc50d
flora.moreau@mmibordeaux.com <FloMoro>
2020-01-17 10:17:44 +0100

    test

--- app/controllers/arts_controller.rb
+++ app/controllers/arts_controller.rb
@@ -71,4 +71,7 @@ class ArtsController < ApplicationController
     def art_params
       params.require(:art).permit(:title, :date, :picture, :description, :artist_id, :type_id, :emotion_id)
     end
+
+
+
 end
